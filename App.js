243b0f0cb8df7c01030ba9e2b017b7a44c9714e6
Sudeep C <sudc@publicisgroupe.net>
2024-01-06 07:31:41 +0530

    test 1

---
+++ App.js
@@ -0,0 +1,49 @@
+
+/*
+ <div id = parent> 
+    <div id= child1>
+        <h1>Im an h1 tag</h1>
+        <h2>Im an h2 tag</h2>
+    </div>
+    <div id= child2>
+        <h1>Im an h1 tag</h1>
+        <h2>Im an h2 tag</h2>
+    </div>
+ </div>
+
+*/
+
+const parent = React.createElement("div", {id:"parent"},
+        [
+            React.createElement("div",{id:"child1"}, [
+            React.createElement("h1",{id:"heading"}, "Im an h1 tag"),
+            React.createElement("h2",{}, "Im an h2 tag")
+        ]
+        ),
+            React.createElement("div",{id:"child2"}, [
+            React.createElement("h1",{}, "Im an h1 tag"),
+            React.createElement("h2",{}, "Im an h2 tag")
+        ]
+        ) ]
+  );
+
+  console.log(parent);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
+
+
+
+  
+/* const heading = React.createElement("h1",{id:heading},"Hi how are you");
+
+// note: const heading = React.createElement(tag,tag props/attributes,children[] );
+
+console.log(heading); // heading is just an  object -> react element beomes html which browser understands
+// holds props and its children,
+ //hold h1 attributes -> id:heading
+
+ // ReactElement(object) => HTML (browser understands)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+ */
---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+Steps to install npm into project:
+
+download nodejs and install
+ from terminal 
+
+npm init 
+
+
+
