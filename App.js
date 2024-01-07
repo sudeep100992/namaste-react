78a06ddc3958f32c3b9bfd667fc6d47fac7f0e05
Sudeep Cheekireddy <sudeep100992@gmail.com>
2024-01-07 17:16:52 +0530

    Ep3: Laying Foundation- JSX , Function Components

--- App.js
+++ App.js
@@ -1,51 +1,43 @@
-
-/*
- <div id = parent> 
-    <div id= child1>
-        <h1>Im an h1 tag</h1>
-        <h2>Im an h2 tag</h2>
-    </div>
-    <div id= child2>
-        <h1>Im an h1 tag</h1>
-        <h2>Im an h2 tag</h2>
-    </div>
- </div>
-
-*/
-import React from "react" ;
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"},
-        [
-            React.createElement("div",{id:"child1"}, [
-            React.createElement("h1",{id:"heading"}, "Im an namaste react 👌😂"),
-            React.createElement("h2",{}, "Im an h2 tag")
-        ]
-        ),
-            React.createElement("div",{id:"child2"}, [
-            React.createElement("h1",{}, "Im an h1 tag"),   
-            React.createElement("h2",{}, "Im an h2 tag")
-        ]
-        ) ]
-  );
+//JSX => React.createElement => object => HTMLElement (render)
+// JSX  and react are separate
+// JSX ->  html and js merge together
+// JSX -> html like or xml like syntax
 
-  console.log(parent);
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(parent);
+//React ELement
+// className="heading" tabIndex="5" learn whats this
+const reactElement = 
+  <h1 id="heading" className="heading" tabIndex="5">
+    reactElement 
+  </h1>
+;
 
+// Fucntional Component
+const Title = ()=>(
+  <h1 id="heading" className="heading" tabIndex="5">
+    JSX Title 💕
+  </h1>
+);
 
+// Fucntional Component
+// component composition
+// different ways to invoke component composition
+// {} -> write JS expression to execute -> like react element , functional component
+const number =7;
 
-  
-/* const heading = React.createElement("h1",{id:heading},"Hi how are you");
+const HeadingComponent = () => (
+  <div id="container">
+    {reactElement}
+    {Title()}
+    <Title></Title>
+    <Title/>
+    {number}   
+  <h1 id="heading" className="headding"> This is Functional Component 😂</h1>
+  </div>
+);
 
-// note: const heading = React.createElement(tag,tag props/attributes,children[] );
-
-console.log(heading); // heading is just an  object -> react element beomes html which browser understands
-// holds props and its children,
- //hold h1 attributes -> id:heading
-
- // ReactElement(object) => HTML (browser understands)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
- */
+root.render(<HeadingComponent/>);
